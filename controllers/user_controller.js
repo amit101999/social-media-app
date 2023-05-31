54bfcc9b208ed03996912b96239d076900fd3298
atulthakur <atulthakur10sep@gmail.com>
2023-05-31 15:09:30 +0530

    create user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,7 @@
 // module.exports.function name is same as exports.function
 
+const User = require("../models/user");
+
 module.exports.profile = (req, res) => {
   return res.render("user_profile");
 };
@@ -10,13 +12,26 @@ module.exports.signUp = (req, res) => {
 };
 
 //render singIn page
-exports.signIn = (req, res) => {
+exports.signIn = async (req, res) => {
   return res.render("user_sign_in");
 };
 
 //creating user
 exports.createUser = async (req, res) => {
-  return res.send("get user");
+  if (req.body.password != req.body.confirm_password) {
+    console.log("password not matched");
+    return res.redirect("back");
+  }
+
+  const checkEmail = await User.findOne({ email: req.body.email });
+  if (checkEmail) {
+    // checking email is already present or not
+    console.log("user already exists with email");
+    return res.redirect("back");
+  }
+  const user = await User.create(req.body);
+  console.log("user created");
+  return res.redirect("/user/sign-in");
 };
 
 exports.createSession = async (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { profile, signIn, signUp } = require("../controllers/user_controller");
+const {
+  profile,
+  signIn,
+  signUp,
+  createUser,
+} = require("../controllers/user_controller");
 
 router.get("/profile", profile);
 router.get("/sign-in", signIn);
 router.get("/sign-up", signUp);
+router.post("/create", createUser);
 
 module.exports = router;
