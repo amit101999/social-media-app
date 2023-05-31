41e2976511abbc71cea8df0c7021c3893cd21991
atulthakur <atulthakur10sep@gmail.com>
2023-05-31 15:55:36 +0530

    signIn page created

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,8 +2,23 @@
 
 const User = require("../models/user");
 
-module.exports.profile = (req, res) => {
-  return res.render("user_profile");
+module.exports.profile = async (req, res) => {
+  try {
+    if (req.cookies.user_id) {
+      const user = await User.findOne({ _id: req.cookies.user_id });
+      if (!user) {
+        console.log("user id cookies is wrong");
+        return res.redirect("/user/sign-in");
+      }
+      console.log("user found : ", user);
+      return res.render("user_profile");
+    } else {
+      console.log("no cookie present that is user is not logged in");
+      return res.redirect("/user/sign-in");
+    }
+  } catch (err) {
+    console.log("error in loading profile page : ", err);
+  }
 };
 
 //render singUp page
@@ -26,7 +41,7 @@ exports.createUser = async (req, res) => {
   const checkEmail = await User.findOne({ email: req.body.email });
   if (checkEmail) {
     // checking email is already present or not
-    console.log("user already exists with email");
+    console.log("user already exists with current email");
     return res.redirect("back");
   }
   const user = await User.create(req.body);
@@ -35,5 +50,26 @@ exports.createUser = async (req, res) => {
 };
 
 exports.createSession = async (req, res) => {
-  return res.send("session created");
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      // checking if user is present with email  or not
+      console.log("user not found");
+      return res.redirect("back");
+    }
+    if (user) {
+      //if user is present with email
+      if (req.body.password !== user.password) {
+        // checking password
+        console.log("password is inCorrect");
+        return res.redirect("back");
+      }
+      console.log("user found and password is Correct");
+      res.cookie("user_id", user._id); // cstroing user id as cookie
+      return res.redirect("/user/profile");
+    }
+  } catch (err) {
+    console.log("error in finding user : ", err);
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,13 @@ const {
   signIn,
   signUp,
   createUser,
+  createSession,
 } = require("../controllers/user_controller");
 
 router.get("/profile", profile);
 router.get("/sign-in", signIn);
 router.get("/sign-up", signUp);
 router.post("/create", createUser);
+router.post("/create-session", createSession);
 
 module.exports = router;
